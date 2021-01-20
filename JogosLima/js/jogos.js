7402edb3d1b8d91386882cd7c7b5e772a6016526
Matheus Machado de Oliveira <77712141+mathxusohai@users.noreply.github.com>
2021-01-20 01:39:23 -0300

    Add files via upload

---
+++ JogosLima/js/jogos.js
@@ -0,0 +1,128 @@
+
+var Ponto = 1
+
+function verificar1(){
+        let resposta1 = document.getElementById("certa1").checked
+        if (resposta1 == true) {
+        document.getElementById("acertou1").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou1").textContent = `Que pena :/ Você errou.`
+        }
+}
+function verificar2(){
+        let resposta2 = document.getElementById("certa2").checked
+        if (resposta2 == true) {
+        document.getElementById("acertou2").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou2").textContent = `Que pena :/ Você errou.`
+        }
+}
+function verificar3(){
+        let resposta3 = document.getElementById("certa3").checked
+        if (resposta3 == true) {
+        document.getElementById("acertou3").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou3").textContent = `Que pena :/ Você errou.`
+        }
+}
+function verificar4(){
+        let resposta4 = document.getElementById("certa4").checked
+        if (resposta4 == true) {
+        document.getElementById("acertou4").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou4").textContent = `Que pena :/ Você errou.`
+        }
+}
+function verificar5(){
+        let resposta5 = document.getElementById("certa5").checked
+        if (resposta5 == true) {
+        document.getElementById("acertou5").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou5").textContent = `Que pena :/ Você errou.`
+        }
+}
+function verificar6(){
+        let resposta6 = document.getElementById("certa6").checked
+        if (resposta6 == true) {
+        document.getElementById("acertou6").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou6").textContent = `Que pena :/ Você errou.`
+        }
+}
+function verificar7(){
+        let resposta7 = document.getElementById("certa7").checked
+        if (resposta7 == true) {
+        document.getElementById("acertou7").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou7").textContent = `Que pena :/ Você errou.`
+        }
+}
+function verificar8(){
+        let resposta8 = document.getElementById("certa8").checked
+        if (resposta8 == true) {
+        document.getElementById("acertou8").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou8").textContent = `Que pena :/ Você errou.`
+        }
+}
+        function verificar9(){
+                let resposta9 = document.getElementById("certa9").checked
+                if (resposta9 == true) {
+                document.getElementById("acertou9").textContent = `Parábens! Você acertou.`
+                document.getElementById("spanPonto").textContent = Ponto + "/12"
+                Ponto = Ponto + 1
+                } else {
+                        document.getElementById("acertou9").textContent = `Que pena :/ Você errou.`
+                }
+        }
+
+function verificar10(){
+        let resposta10 = document.getElementById("certa10").checked
+        if (resposta10 == true) {
+        document.getElementById("acertou10").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou10").textContent = `Que pena :/ Você errou.`
+        }
+}
+function verificar11(){
+        let resposta11 = document.getElementById("certa11").checked
+        if (resposta11 == true) {
+        document.getElementById("acertou11").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou11").textContent = `Que pena :/ Você errou.`
+        }
+}
+function verificar12(){
+        let resposta12 = document.getElementById("certa12").checked
+        if (resposta12 == true) {
+        document.getElementById("acertou12").textContent = `Parábens! Você acertou.`
+        document.getElementById("spanPonto").textContent = Ponto + "/12"
+        Ponto = Ponto + 1
+        } else {
+                document.getElementById("acertou12").textContent = `Que pena :/ Você errou.`
+        }
+}
+function recarregar(){
+        window.location.reload()
+        window.scrollTo(0,0)
+}
